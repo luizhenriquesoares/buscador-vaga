67d7c260cf297b8937fb6240ba1ad6673e53b450
Luiz Henrique <luizhenriquesoares91@gmail.com>
2018-01-20 15:54:47 -0300

    import rest-client

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const { Client } = require("node-rest-client");
+
+// create instance client
+const client = new Client({});
 
 exports.getAllUsers = (req, res) => {
   res.json("all");
